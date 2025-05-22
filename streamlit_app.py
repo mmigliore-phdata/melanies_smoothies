7034a03f5b25b1640bdf4a51166ce4efdbd451db
mmigliore-phdata <mmigliore@phdata.io>
2025-05-22 11:06:33 -0500

    Update streamlit_app.py
    Fix =

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 #Convert the Snowpark Dataframe to Pandas to yes LOC function
-pd_df-my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
